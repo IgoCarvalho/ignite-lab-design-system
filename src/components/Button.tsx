dadddf21e86bf04f23172330689d94038dc6e070
Igo Carvalho <igocarvalho00@gmail.com>
2023-01-11 05:50:54 -0300

    ✨ create App page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,27 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ asChild, children }: ButtonProps) {
+export function Button({
+  asChild,
+  children,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       className={clsx(
-        'px-4 py-3 w-full bg-cyan-500 hover:bg-cyan-300 rounded font-semibold text-black text-sm transition-colors focus:ring ring-white'
+        'px-4 py-3 w-full bg-cyan-500 hover:bg-cyan-300 rounded font-semibold text-black text-sm transition-colors focus:ring ring-white',
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
