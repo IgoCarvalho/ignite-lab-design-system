5ac19c6d68a2d76a26521fb7347ec00418409bf1
Igo Carvalho <igocarvalho00@gmail.com>
2023-01-12 18:30:02 -0300

    ✅ create SignIn page test within storybook

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import { SignIn } from './SignIn';
 
@@ -9,4 +11,20 @@ export default {
 
 type SignInStory = StoryObj;
 
-export const Default: SignInStory = {};
+export const Default: SignInStory = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText('jhondoe@example.com'),
+      'igocarvalho@gmail.com'
+    );
+    userEvent.type(canvas.getByPlaceholderText('**********'), '1234567890');
+
+    userEvent.click(canvas.getByRole('button'));
+
+    await waitFor(() =>
+      expect(canvas.getByText('Login realizado! 🎉')).toBeInTheDocument()
+    );
+  },
+};
